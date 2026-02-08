3b750b6a98f102bb0b590b0e55a9d11aebfe4c65
Rockstatata <sarwad015@gmail.com>
2026-02-08 09:11:15 +0600

    feat: initial commit

---
+++ ClassWiz/ClassWizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassWizApp.swift
+//  ClassWiz
+//
+//  Created by Sarwad Hasan  on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ClassWizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
